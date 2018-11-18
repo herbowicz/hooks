613bdcea23c69d0ba866dd29dd3f3adb653a2ca2
Greg Herbowicz <herbowicz@gmail.com>
2018-11-18 09:50:50 +0100

    hooks counter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './main.css';
+
+function Counter({ initialCount }) {
+    const [count, setCount] = useState(initialCount);
+    return (
+        <>
+            Count: {count}
+            <br />
+            <br />
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>
+                +
+            </button>
+            <button onClick={() => setCount(prevCount => prevCount - 1)}>
+                -
+            </button>
+        </>
+    );
+}
+
+function App() {
+    return (
+        <div className="App">
+            <h1>React Hooks Example</h1>
+            <Counter initialCount={0} />
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
