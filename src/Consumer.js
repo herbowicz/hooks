a5e2f0c006a8654038835393e2830d4273c6417b
Greg Herbowicz <herbowicz@gmail.com>
2019-03-30 17:33:45 +0100

    add fetch

--- src/Consumer.js
+++ src/Consumer.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { MyContext } from './Context';
 
-
 const Consumer = () => {
     const value = useContext(MyContext);
     return <div>{value} is from Context.</div>;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import CountDown from './CountDown'
 import Counter from './Counter'
 import CustomInput from './CustomInput'
 import Context from './Context'
+import Fetch from './Fetch'
 
 const App = () => <>
     <CountDown /><hr />
     <Counter /><hr />
     <CustomInput /><hr />
     <Context /><hr />
+    <Fetch /><hr />
 </>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
