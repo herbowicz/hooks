b0816f7262d2c62483882b603bc7037fa515bbf3
Greg Herbowicz <herbowicz@gmail.com>
2019-03-30 12:43:36 +0100

    add simple context example

---
+++ src/Consumer.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { MyContext } from './Context';
+
+
+const Consumer = () => {
+    const value = useContext(MyContext);
+    return <div>{value} is from Context.</div>;
+}
+
+export default Consumer;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 
 import CountDown from './CountDown'
 import Counter from './Counter'
+import CustomInput from './CustomInput'
+import Context from './Context'
 
 const App = () => <>
     <CountDown /><hr />
     <Counter /><hr />
+    <CustomInput /><hr />
+    <Context /><hr />
 </>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
