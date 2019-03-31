4242ff30833e52a2e869b1de6cd3aa522735af01
Greg Herbowicz <herbowicz@gmail.com>
2019-03-31 12:39:17 +0200

    context2

--- src/Context.js
+++ src/Context.js
@@ -1,15 +1,14 @@
 import React, { createContext } from "react";
-import Consumer from './Consumer'
+import Consumer from "./Consumer";
 
 export const MyContext = createContext();
 
 function Context() {
-    return (
-        <MyContext.Provider value={"My value"}>
-            <Consumer />
-        </MyContext.Provider>
-    );
+  return (
+    <MyContext.Provider value={"My value"}>
+      <Consumer />
+    </MyContext.Provider>
+  );
 }
-
 
 export default Context;
